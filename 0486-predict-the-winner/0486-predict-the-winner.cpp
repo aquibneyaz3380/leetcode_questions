cfdad6521288bd7d7fc985fef83d452ddfb9a34a
aquib <aquibneyaz3380@gmail.com>
2026-08-02 12:59:34 +0530

    Time: 99 ms (15.03%), Space: 9.4 MB (96.55%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -1,19 +1,14 @@
 class Solution {
 public:
-    bool predictTheWinner(vector<int>& A) {
-        int n = A.size();
-        if (~n & 1) return true;
-
-        vector dp(n, vector<int>(n, -1));
-        auto maxDiff = [&](this auto&& maxDiff, int i, int j) -> int {
-            int& res = dp[i][j];
-            if (res != -1) return res;
-            if (i == j) return res = A[i];
-
-            return res = max(A[i] - maxDiff(i + 1, j),
-                             A[j] - maxDiff(i, j - 1));
-        };
-
-        return maxDiff(0, n - 1) >= 0;
+  int solve(int i,int j,vector<int>& nums){
+    if(i>j)return 0;
+    if(i==j)return nums[i];
+    int take_i=nums[i]-solve(i+1,j,nums);
+    int take_j=nums[j]-solve(i,j-1,nums);
+    return max(take_i,take_j);
+ }
+    bool predictTheWinner(vector<int>& nums) {
+        int n =nums.size();
+        return solve(0,n-1,nums)>=0;
     }
 };
