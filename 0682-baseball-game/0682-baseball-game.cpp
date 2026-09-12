29d3bfd9e51ae8bc5417a1b27e2f6a4c27a117cd
aquib <aquibneyaz3380@gmail.com>
2026-09-12 19:46:51 +0530

    Time: 0 ms (100%), Space: 13.1 MB (16.34%) - LeetHub

---
+++ 0682-baseball-game/0682-baseball-game.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int calPoints(vector<string>& operations) {
+        stack<int> st;
+
+        for (int i = 0; i < operations.size(); i++) {
+
+            if (operations[i] == "C") {
+                st.pop();
+            }
+            else if (operations[i] == "+") {
+                int first = st.top();
+                st.pop();
+
+                int second = st.top();
+
+                st.push(first);
+                st.push(first + second);
+            }
+            else if (operations[i] == "D") {
+                st.push(st.top() * 2);
+            }
+            else {
+                st.push(stoi(operations[i]));
+            }
+        }
+
+        int sum = 0;
+
+        while (!st.empty()) {
+            sum += st.top();
+            st.pop();
+        }
+
+        return sum;
+    }
+};
